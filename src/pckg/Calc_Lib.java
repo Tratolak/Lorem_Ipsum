574884cc0b472b2c72e6aaee86cb029eafa08ed9
Ondraol <ondra.olsak@gmail.com>
2017-04-09 11:56:10 +0200

    Test modification, mat. lib. (factorial, divide) + comments

--- src/pckg/Calc_Lib.java
+++ src/pckg/Calc_Lib.java
@@ -6,12 +6,12 @@ import java.util.*;
 /**
  * Matematicka knihovna obsahujici funkce pro vypocet: souctu, rozdilu,
  * soucinu, podilu, faktorialu, mocniny, odmocniny a smerodatne odchylky.
- * V pripade chyby vraci kazda z funkci vyjimku.
+ * V pripade chyby vyvola kazda z funkci vyjimku.
  */
 public class Calc_Lib {
 
 	/**
-	 * Provadi soucet dvou realnych cisel. V pripade preteceni vraci vyjimku.
+	 * Provadi soucet dvou realnych cisel. V pripade preteceni vyvola vyjimku.
 	 *
 	 * @param x double
 	 * @param y double
@@ -27,7 +27,7 @@ public class Calc_Lib {
 	}
 
 	/**
-	 * Provadi rozdil dvou realnych cisel. V pripade preteceni vraci vyjimku.
+	 * Provadi rozdil dvou realnych cisel. V pripade preteceni vyvola vyjimku.
 	 *
 	 * @param x double
 	 * @param y double
@@ -43,7 +43,7 @@ public class Calc_Lib {
 	}
 
 	/**
-	 * Provadi nasobeni dvou realnych cisel. V pripade preteceni vraci vyjimku.
+	 * Provadi nasobeni dvou realnych cisel. V pripade preteceni vyvola vyjimku.
 	 *
 	 * @param x double
 	 * @param y double
@@ -58,30 +58,62 @@ public class Calc_Lib {
 		return result;
 	}
 
-	public static double divide(double x, double y) {
+	/**
+	 * Provadi vypoce podilu dvou cisel. Pokud je delitel 0 vyvola vyjimku
+	 * @param x - delenec
+	 * @param y - delitel
+	 * @return vysledek po nasobeni
+	 * @throws Exception
+	 */
+	public static double divide(double x, double y) throws Exception{
+		if(y == 0){
+			throw new Exception("Dividing by zero");
+		}
 		return x/y;
 	}
 
-	public static long factorial(long x) {
-		return 1;
+	/**
+	 * Provadi vypocet faktorialu. Pokud je cislo 'x' zaporne nebo dojde k preteceni vyvola vyjimku.
+	 * @param x - cislo pro vypocet faktorialu
+	 * @return hdnotu faktorialu
+	 * @throws Exception
+	 */
+	public static long factorial(long x) throws Exception{
+		if(x < 0){
+			throw  new Exception("Factorial from negative number");
+		}
+		long result = 1;
+		do{
+			if(result >= Long.MAX_VALUE / x)
+			{
+				throw new Exception("Factorial overflow");
+			}
+			result = result * x;
+			x-- ;
+		}while(x > 0);
+		return result;
 	}
 
 	/**
-	 * Counts power with Taylor's series
-	 * @param x - base
+	 * Provadi vypocet mocniny pomoci Taylorova polynomu. Pokud je zaklad zaporne cislo, provede se kontrola jestli je
+	 * exponent sude nebo liche cislo. Pokud je exponent sudy, zaklad je vynasoben -1 a nasledne je proveden vypocet.
+	 * Pokud je exponent lichy aplikuje se stejny postu a na zaver je vysledek vynasoben -1. Pokud exponent neni cele
+	 * cislo je vysledek roven hodnote NaN.
+	 *
+	 * @param x - zaklad
 	 * @param n - exponent
-	 * @return powered number
+	 * @return umocnene cislo
 	 * @throws Exception
 	 */
 	public static double power(double x, double n) throws Exception {
-		boolean neg_flag = false; //detect if ne is  negative
-		double step = 1; //part of polynomial
+		boolean neg_flag = false; //
+		double step = 1; //cast polynomu
 		double prev_result = 1;
 		double result = 0;
 		int iterator = 1;
-		//power of negative number
-		if (x < 0 && n % 1 == 0) { //check if base is negative and exponent is int type
-			if (n % 2 != 0) //check if number is even
+		//mocnina se zaporneho cisla
+		if (x < 0 && n % 1 == 0) { //kotrola jestli je zaklad zaporny a exponent je typu int
+			if (n % 2 != 0) //kontrola jestli je cislo sude
 				neg_flag = true;
 			x *= (-1);
 		}
@@ -95,7 +127,7 @@ public class Calc_Lib {
 		if (result == Double.POSITIVE_INFINITY) {
 			throw new Exception("Power overflow");
 		} else {
-			if (neg_flag)
+			if (neg_flag) //kontrola jestli byl zaklad zaporne cislo
 				return result * (-1);
 			else
 				return result;
@@ -103,11 +135,11 @@ public class Calc_Lib {
 	}
 
 	/**
-	 * Counts root n^sqrt(x) and returns result. If x<0 or n ==0 throws exception.
+	 * Provadi vypocet odmocniny n^sqrt(x). Jestlize je x<0 nebo n==0 vyvola vyjimku.
 	 *
-	 * @param x - root base
-	 * @param n - root stage
-	 * @return result of root
+	 * @param x - zaklad
+	 * @param n - stupen odmocniny
+	 * @return vysledek po odmocnovani
 	 * @throws Exception
 	 */
 	public static double root(double x, double n) throws Exception {
@@ -130,33 +162,34 @@ public class Calc_Lib {
 		}
 		return result;
 	}
-	
+
 	/**
 	 * Vypočítá směrodatnou odchylku z hodnot, které se nacházejí v souboru zadaném cestou 'filename'.
 	 * Čísla uvnitř souboru jsou ve fromátu: cislo1 'whitespace chars' cislo2' whitespace chars'... cislo n
 	 * Směrodatnou odchylku počítá podle vzorečku: ((suma(od i=0 do n)(xi - Xprumer)^2)/(n-1))^0.5
+	 *
 	 * @param filename - Cesta k souboru s čísly
 	 * @return hodnota směrodatné odchylky
 	 * @throws Exception
 	 */
 	public static double st_Dev(String filename) throws Exception {
 		List<Double> doubles = new ArrayList<>();
-		
+
 		BufferedReader br = new BufferedReader(new FileReader(filename));
-		try 
+		try
 		{
 		    String line = br.readLine();
 		    String StringArr[];
 
-		    while (line != null) 
+		    while (line != null)
 		    {
 		    	StringArr = line.split("\\s+");
-		    	
+
 		    	for(String item : StringArr)
 		    	{
 		    		doubles.add(Double.parseDouble(item));
 		    	}
-		        
+
 		        line = br.readLine();
 		    }
 		}
@@ -164,14 +197,14 @@ public class Calc_Lib {
 		{
 			throw new Exception("Invalid input file");
 		}
-		finally 
+		finally
 		{
 		    br.close();
 		}
-		
+
 		//pocitani prumeru
 		double average = 0.0;
-		
+
 		for(Double item : doubles)
 		{
 			try
@@ -183,7 +216,7 @@ public class Calc_Lib {
 				throw e;
 			}
 		}
-		
+
 		try
 		{
 			average = divide(average, doubles.size());
@@ -192,9 +225,9 @@ public class Calc_Lib {
 		{
 			throw e;
 		}
-		
+
 		double sum = 0.0;
-		
+
 		//pocitani suma(0-n)(xi - Xprumer)^2
 		for(Double item : doubles)
 		{
@@ -207,7 +240,7 @@ public class Calc_Lib {
 				throw e;
 			}
 		}
-		
+
 		//vysledek (suma/n-1)^0.5
 		return root(divide(sum, doubles.size()-1),2);
 	}
