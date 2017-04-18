08ac764b916a372e25bf4ddd94f49ca68d1fc737
Ondraol <ondra.olsak@gmail.com>
2017-04-18 06:10:00 +0200

    Tests - functions call repair

--- src/pckg/Calc_LibTest.java
+++ src/pckg/Calc_LibTest.java
@@ -1,7 +1,6 @@
 package pckg;
 
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
@@ -10,21 +9,13 @@ import static org.junit.Assert.*;
 
 public class Calc_LibTest {
 
-    private Calc_Lib calc;
-
     @Rule
     public ExpectedException thrown = ExpectedException.none();
-
-    @Before
-    public void init(){
-       calc = new Calc_Lib();
-
-    }
 
     @Test
     public void add() throws Exception{
         try{
-            calc.add(Double.MAX_VALUE, Double.MAX_VALUE);
+            Calc_Lib.add(Double.MAX_VALUE, Double.MAX_VALUE);
             fail();
         } catch (Exception e){
             final String expected = "Add overflow";
@@ -32,14 +23,14 @@ public class Calc_LibTest {
         }
 
         try{
-            calc.add(-Double.MAX_VALUE, -Double.MAX_VALUE);
+            Calc_Lib.add(-Double.MAX_VALUE, -Double.MAX_VALUE);
             fail();
         } catch (Exception e){
             final String expected = "Add overflow";
             assertEquals(expected, e.getMessage());
         }
 
-        double result = calc.add(-105.4,45.6);
+        double result = Calc_Lib.add(-105.4,45.6);
         Assert.assertEquals(-59.8, result,0.0001);
     }
 
@@ -47,7 +38,7 @@ public class Calc_LibTest {
     @Test
     public void sub() throws Exception {
 		try{
-			calc.sub(-Double.MAX_VALUE, Double.MAX_VALUE);
+			Calc_Lib.sub(-Double.MAX_VALUE, Double.MAX_VALUE);
 			fail();
 		} catch (Exception e){
 			final String expected = "Sub overflow";
@@ -55,14 +46,14 @@ public class Calc_LibTest {
 		}
 
 		try{
-			calc.sub(Double.MAX_VALUE, -Double.MAX_VALUE);
+			Calc_Lib.sub(Double.MAX_VALUE, -Double.MAX_VALUE);
 			fail();
 		} catch (Exception e){
 			final String expected = "Sub overflow";
 			assertEquals(expected, e.getMessage());
 		}
 
-		double result = calc.sub(105.4,45.6);
+		double result = Calc_Lib.sub(105.4,45.6);
 		Assert.assertEquals(59.8, result,0.0001);
     }
 
@@ -70,114 +61,114 @@ public class Calc_LibTest {
     @Test
     public void mult() throws Exception {
 		try{
-			calc.mult(Double.MAX_VALUE, 2);
+			Calc_Lib.mult(Double.MAX_VALUE, 2);
 			fail();
 		} catch (Exception e){
 			final String expected = "Mult overflow";
 			assertEquals(expected, e.getMessage());
 		}
 		try{
-			calc.mult(Double.MAX_VALUE, -2);
+			Calc_Lib.mult(Double.MAX_VALUE, -2);
 			fail();
 		} catch (Exception e){
 			final String expected = "Mult overflow";
 			assertEquals(expected, e.getMessage());
 		}
 
-		double result = calc.mult(105.4,5);
+		double result = Calc_Lib.mult(105.4,5);
 		Assert.assertEquals(527, result,0);
     }
 
     @Test
     public void divide() throws Exception {
 		try{
-			calc.divide(100, 0);
+			Calc_Lib.divide(100, 0);
 			fail();
 		} catch (Exception e){
 			final String expected = "Dividing by zero";
 			assertEquals(expected, e.getMessage());
 		}
 
-		double result = calc.divide(105.4,6);
+		double result = Calc_Lib.divide(105.4,6);
 		Assert.assertEquals(17.5666, result,0.0001);
     }
 
     @Test
     public void factorial() throws Exception {
 		try{
-			calc.factorial(100);
+			Calc_Lib.factorial(100);
 			fail();
 		} catch (Exception e){
 			final String expected = "Factorial overflow";
 			assertEquals(expected, e.getMessage());
 		}
 		try{
-			calc.factorial(-4);
+			Calc_Lib.factorial(-4);
 			fail();
 		} catch (Exception e){
 			final String expected = "Factorial from negative number";
 			assertEquals(expected, e.getMessage());
 		}
 
-		long result = calc.factorial(8);
+		long result = Calc_Lib.factorial(8);
 		Assert.assertEquals(40320, result);
     }
 
     @Test
     public void power() throws Exception {
 		try{
-			calc.power(Double.MAX_VALUE, 2);
+			Calc_Lib.power(Double.MAX_VALUE, 2);
 			fail();
 		} catch (Exception e){
 			final String expected = "Power overflow";
 			assertEquals(expected, e.getMessage());
 		}
 
-		double result = calc.power(8.5,5.1);
+		double result = Calc_Lib.power(8.5,5.1);
 		Assert.assertEquals(54958.70883, result,0.00001);
 
-		result = calc.power(-4.5,4);
+		result = Calc_Lib.power(-4.5,4);
 		Assert.assertEquals(410.0625, result,0.00001);
 
-		result = calc.power(-8.5,5);
+		result = Calc_Lib.power(-8.5,5);
 		Assert.assertEquals(-44370.53125, result,0.00001);
 
-		result = calc.power(-8.5,5.4);
+		result = Calc_Lib.power(-8.5,5.4);
 		Assert.assertEquals(Double.NaN, result,0);
 	}
 
     @Test
     public void root() throws Exception {
 		try{
-			calc.root(-4, 2);
+			Calc_Lib.root(-4, 2);
 			fail();
 		} catch (Exception e){
 			final String expected = "Root from negative number";
 			assertEquals(expected, e.getMessage());
 		}
 		try{
-			calc.root(4, 0);
+			Calc_Lib.root(4, 0);
 			fail();
 		} catch (Exception e){
 			final String expected = "Can't count zero root";
 			assertEquals(expected, e.getMessage());
 		}
 
-		double result = calc.root(0,4);
+		double result = Calc_Lib.root(0,4);
 		Assert.assertEquals(0, result,0);
 
-		result = calc.root(8.2,-3.1);
+		result = Calc_Lib.root(8.2,-3.1);
 		Assert.assertEquals(0.507249, result,0.00001);
     }
 
     @Test
     public void st_Dev() throws Exception {
 		double result;
-		result = calc.st_Dev("src/pckg/st_Dev_00.txt");
+		result = Calc_Lib.st_Dev("src/pckg/st_Dev_00.txt");
 		Assert.assertEquals(46.58169, result,0.00001);
 
 		try{
-			calc.st_Dev("src/pckg/st_Dev_01.txt");
+			Calc_Lib.st_Dev("src/pckg/st_Dev_01.txt");
 			fail();
 		} catch (Exception e){
 			final String expected = "Invalid input file";
